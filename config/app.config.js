a5953aa2caf701479e09592ee0407358243806b2
RNO <arnaud.juracek@gmail.com>
2019-01-10 16:00:11 +0100

    Allow configuration hot-reloading
    When any `rc` config file is modified, this will:
    + reload any client serve w/ dev-server
    + mutate configuration object on the server
    + Âallow any build client serve w/ server to access ne configuration
    on manual reload

--- config/app.config.js
+++ config/app.config.js
@@ -1,32 +1,61 @@
 const fs = require('fs')
+const parseStringInObject = require('parse-strings-in-object')
 const path = require('path')
 const pckg = require('package')(module)
+const rc = require('rc')
 const resolvePath = require('@server/utils/resolve-configuration-paths')
 
 const appname = pckg.name
 process.title = appname
 
+/**
+ * APP CONFIGURATION
+ * Construct from package.json and .rythmusrc
+ * Overridden in order by :
+ * - cli args `--parent.child=value`
+ * - environment variables `rythmus_parent__child=value`
+ * SEE: https://github.com/dominictarr/rc#standards
+ */
 const defaultConfigPath = path.join(__dirname, '..', '.apprc')
-const defaultConfig = JSON.parse(fs.readFileSync(defaultConfigPath, 'utf-8'))
-const configuration = require('rc')(appname, defaultConfig)
+const readrc = () => {
+  const defaultConfig = JSON.parse(fs.readFileSync(defaultConfigPath, 'utf-8'))
+  return parseStringInObject(rc(appname, defaultConfig))
+}
 
+let configuration = readrc()
 configuration.host.dist = resolvePath(configuration.host.dist)
-
 configuration.appname = appname
 configuration.package = pckg
 configuration.help = configuration.help || configuration.h
 configuration.version = configuration.version || configuration.v
 
-// Show help
+/**
+ * ACTIONS
+ */
 if (configuration.help) {
   console.log(fs.readFileSync(path.join(__dirname, 'USAGE'), 'utf-8'))
   process.exit(0)
 }
 
-// Show version
 if (configuration.version) {
   console.log(configuration.package.version)
   process.exit(0)
+}
+
+/**
+ * HOT RELOADING
+ * only when --hot flag is present
+ */
+if (configuration.hot) {
+  require('chokidar')
+    .watch([defaultConfigPath, ...(configuration.configs || [])])
+    .on('change', () => {
+      try {
+        configuration = Object.assign(configuration, readrc())
+      } catch (err) {
+        console.error(err)
+      }
+    })
 }
 
 module.exports = configuration
