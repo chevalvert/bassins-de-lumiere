edc39d43089917c65fd0172e065b61d967b3fd82
RNO <arnaud.juracek@gmail.com>
2019-01-21 18:34:30 +0100

    wip dataset builder, wip streetview embed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import App from 'layouts/app'
 import load from 'controllers/loader'
+import * as Kaleidoscope from 'kaleidoscopejs'
 
 if (!window.isProduction) {
   require('fps-indicator')({ position: 'bottom-left' })
 }
+
+const viewer = new Kaleidoscope.Image({
+  // SEE: https://github.com/thiagopnts/kaleidoscope#kaleidoscopeimage
+  source: 'datas/vert1/spheres/63.png',
+  container: document.body
+})
+viewer.render()
 
 const app = new App()
 
