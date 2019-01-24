bf39fbe17e0f05152494eb0e09335baee0871c25
RNO <arnaud.juracek@gmail.com>
2019-01-24 17:58:30 +0100

    wip cache

--- src/controllers/cache.js
+++ src/controllers/cache.js
@@ -1,25 +1,45 @@
+import Emitter from 'tiny-emitter'
+
 /* global Image */
 
+const events = new Emitter()
 const NS = '__CACHE.'
 const cached = {}
+const queued = {}
+let queueIsProcessing = false
 
 if (!window.isProduction) {
   window.cache = cached
 }
 
-function load (url) {
+const queue = url => new Promise(resolve => {
+  queued[url] = url
   cached[NS + url] = new Image()
+  if (!queueIsProcessing) processQueue()
+  events.once(url, resolve)
+})
+
+const load = async url => new Promise(resolve => {
+  cached[NS + url].onload = function () {
+    events.emit(url)
+    resolve()
+  }
   cached[NS + url].src = url
+})
+
+async function processQueue () {
+  document.body.classList.add('is-loading')
+  while (Object.keys(queued).length) {
+    queueIsProcessing = true
+    const current = Object.keys(queued)[0]
+    await load(current)
+    delete queued[current]
+  }
+  queueIsProcessing = false
+  document.body.classList.remove('is-loading')
 }
 
 export default {
-  load: (...urls) => {
-    // TODO: queue calls instead of parallel
-    urls.forEach(load)
-  },
-
-  get: url => {
-    if (!cached[NS + url]) load(url)
-    return cached[NS + url]
-  }
+  load: (...urls) => urls.forEach(queue),
+  get: url => cached[NS + url]
 }
