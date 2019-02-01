26bcbc6e63bf1d10aea7658815756fdbece79d95
RNO <arnaud.juracek@gmail.com>
2019-02-01 11:07:52 +0100

    Add `window.debugLight(index)` function
    This function is for debug purpose only, and shouldn't be use
    outside devtools console

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import 'controllers/single-point-mode'
 import 'controllers/hardware'
+import 'controllers/single-point-mode'
 import App from 'layouts/app'
 import iddle from 'controllers/iddle'
 import load from 'controllers/loader'
+import store from 'controllers/store'
 
 if (!window.isProduction) {
   require('fps-indicator')({ position: 'top-right', color: 'white' })
+
+  // NOTE: this function shouldn't be used outside devtools console
+  window.debugLight = index => {
+    store.set('singlePointMode', true)
+    store.set('maxProgressPercent', index / store.get('points').length)
+    return index
+  }
 }
 
 const app = new App()
